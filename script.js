c35c6da5d23c23d1fae89155a162c535de045378
Ilakian Puvanendra <81735782+Ilakiancs@users.noreply.github.com>
2024-06-27 02:20:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,7 @@ function generateQRCode() {
         return;
     }
 
-    var cardContent = `Name: ${name}<br>Email: ${email}`;
-    document.getElementById('card').innerHTML = cardContent;
-
-    // Update this URL to your Netlify site URL
-    var qrCodeData = `https://merry-tartufo-fd5d9e.netlify.app/`;
+    var qrCodeData = `card.html?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`;
     
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         text: qrCodeData,
